1471acd5f528edffa11ad903ba6fb68cdeb619b9
Ayush Mudgal <ayushincnca@gmail.com>
2015-08-21 10:46:50 -0700

    modified commit file

---
+++ LuminousTests/LuminousTests.m
@@ -0,0 +1,34 @@
+//
+//  LuminousTests.m
+//  LuminousTests
+//
+//  Created by Aditya Challa on 4/22/14.
+//  Copyright (c) 2014 BerkeleyBoys. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LuminousTests : XCTestCase
+
+@end
+
+@implementation LuminousTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
